81030434bcaac26323d9121d18e5272e0bdd4aed
Diego Rodrigo <166569856+diegoLm011@users.noreply.github.com>
2024-04-10 17:41:24 -0300

    Add files via upload

---
+++ ProjetoA3_BlueWorld/src/main/java/br/com/bossini/_usjt_psc_quinta_projeto_eventos/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bossini._usjt_psc_quinta_projeto_eventos;
+
+/**
+ *
+ * @author //Grupo 04\\
+ */
+public class App {
+
+    public static void main(String[] args) {
+        // Codigo aq
+    }
+}
