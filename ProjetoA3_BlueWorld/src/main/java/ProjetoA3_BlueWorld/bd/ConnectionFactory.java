81f6478f0de0bb33af9c18894c3ef71d13995287
Saulo <saulolduarte@outlook.com>
2024-06-05 13:08:48 -0300

    Arruma a estrutura da classe ConnectionFactory

--- ProjetoA3_BlueWorld/src/main/java/ProjetoA3_BlueWorld/bd/ConnectionFactory.java
+++ ProjetoA3_BlueWorld/src/main/java/ProjetoA3_BlueWorld/bd/ConnectionFactory.java
@@ -2,31 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+package ProjetoA3_BlueWorld.bd;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Map;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionFactory {
     // Método para criar a conexão
     public Connection conectar() throws Exception {
-        // Carrega as variáveis de ambiente do arquivo .env
+        // Carregando as variáveis de ambiente do arquivo .env
         Dotenv dotenv = Dotenv.load();
 
-        // Recupera as variáveis de ambiente do arquivo .env
+        // Buscando as variáveis de ambiente do arquivo .env
         String host = dotenv.get("HOST");
         String port = dotenv.get("PORT");
         String user = dotenv.get("USER");
         String password = dotenv.get("PASSWORD");
         String database = dotenv.get("DATABASE");
 
-        // Monta a string de conexão
+        // Montando a string de conexão
         String stringConexao = String.format(
             "jdbc:postgresql://%s:%s/%s",
             host, port, database
         );
 
-        // Retorna a conexão
         return DriverManager.getConnection(stringConexao, user, password);
     }
 }
